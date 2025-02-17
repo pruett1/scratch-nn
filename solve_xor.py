be62023e68bda62c5f7f52a2489e4b5842a350ec
Oliver Pruett <ospruett19@gmail.com>
2025-02-17 12:09:47 -0600

    Added solve xor for scratch nn

---
+++ solve_xor.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from neural_net.network import Network
+from neural_net.layers.fc_layer import FCLayer
+from neural_net.layers.act_layer import ActivationLayer
+
+# training data
+x_train = np.array([[[0,0]], [[0,1]], [[1,0]], [[1,1]]])
+y_train = np.array([[0], [1], [1], [0]])
+
+net = Network()
+net.add(FCLayer(2, 3))
+net.add(ActivationLayer('tanh'))
+net.add(FCLayer(3, 12))
+net.add(ActivationLayer('tanh'))
+net.add(FCLayer(12, 24))
+net.add(ActivationLayer('tanh'))
+net.add(FCLayer(24, 12))
+net.add(ActivationLayer('relu'))
+net.add(FCLayer(12, 3))
+net.add(ActivationLayer('tanh'))
+net.add(FCLayer(3, 1))
+net.add(ActivationLayer('sigmoid'))
+
+net.loss_type('mse')
+net.fit(x_train, y_train, epochs=1000, learning_rate=0.1)
+
+out = net.predict(x_train)
+print(out)
